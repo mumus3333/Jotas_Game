a6b761b2a1ecdd7574d768c2e4376fca4db9a562
Eduardo Ramirez <2230159@upv.edu.mx>
2024-03-08 11:06:33 -0600

    comi

---
+++ src/main/java/org/example/GamePanel.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+
+    //Configuraciones de pantalla
+
+    final int originalTileSize = 16; // 16 x 16 cuadros
+
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.swing.*;
+
+
+public class Main {
+
+    public static void main(String[] args){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("THE DARK GAME");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+
+
+}
