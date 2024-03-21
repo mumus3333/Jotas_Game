b678ef1aa1e49c1077f2ec696d46a539c8b9855d
Eduardo Ramírez Carrillo <156376874+mumus3333@users.noreply.github.com>
2024-03-21 10:37:50 -0600

    pepe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("THE DARK GAME");
+        window.setTitle("THE UPV FANTASY");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
